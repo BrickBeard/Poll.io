aaf30a7fc517de3ef89969cb0f132168d17f5721
BrickBeard <brickbeard.io@gmail.com>
2018-09-04 13:40:02 -0500

    Added the React features to the front-end

---
+++ static/js/polls.js
@@ -0,0 +1,102 @@
+// CSS style to align text to the center of it's container
+
+var Align = {
+    textAlign: 'center',
+    fontFamily: 'EB Garamond'
+};
+
+var PollForm = React.createClass({
+    
+    getInitialState: function(){
+        // Set initial state of form inputs
+        return {title: '', option: '', options: []}
+    },
+    
+    handleTitleChange: function(e){
+        // Change title as the user types
+        this.setState({title: e.target.value});
+    },
+    
+    handleOptionChange: function(e){
+        this.setState({option: e.target.value});
+    },
+    
+    handleOptionAdd: function(e){
+        // Update poll options and reset options to an empty string
+        this.setState({
+            options: this.set.options.concat({name: this.state.option}),
+            option: ''
+        });
+    },
+    
+    handleSubmit: function(e){
+        // TODO handle form submit
+        e.preventDefault();
+    },
+    
+    render: function(){
+        return (
+            <div>
+                <form className="form-signin" id="poll_form" onSubmit={this.handleSubmit}>
+                    <h2 className="form-signin-heading" style={Align}>Create a Poll</h2>
+                    
+                    <div className="form-group has-success">
+                        <label htmlFor="title" className="sr-only">Title</label>
+            <input type="text" id="title" name="title" className="form-control" placeholder="Title" onChange={this.handleTitleChange} required autoFocus />
+                    </div>
+            
+                    <div className="form-group has-success">
+                        <label htmlFor="option" className="sr-only">Option</label>
+            <input type="text" id="option" name="option" className="form-control" placeholder="Option" onChange={this.handleOptionChange} required autoFocus />
+                    </div>
+            
+                    <div className="form-group row">
+                        <button className="btn btn-lg btn-success btn-block" onClick={this.handleOptionAdd}>Add Option</button>
+                        <button className="btn btn-lg btn-success btn-block" type="submit">Save Poll</button>
+                    </div>
+                    <br/>
+                </form>
+                <h3 style={Align}>Live Preview</h3>
+                <LivePreview options={this.state.options} />
+            </div>
+        );
+    }
+});
+
+var LivePreview = React.createClass({
+    render: function(){
+        var options = this.props.options.map(function(option){
+            
+            if(option.name) {
+                return (
+                    <div key={option.name}>
+                        <input type="radio" name={option.name} value={option.name} /> {option.name}
+                        <br/>
+                    </div>
+                );
+            }
+        });
+        
+        return (
+            <div className="panel panel-success">
+                <div className="panel-heading">
+                    <h4>{this.props.title}</h4>
+                </div>
+                <div className="panel-body">
+                    <form>
+                        {options}
+                        <br/>
+                        <button type="submit" className="btn btn-success btn-outline hvr-grow">Vote!</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <div>
+        <PollForm />
+    </div>
+    document.getElementById('form_container')
+);
